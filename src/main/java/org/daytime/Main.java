31fc0a643807b01b8597dd3b06ef82582a01f885
igorbratus <brat.igor1992@gmail.com>
2023-01-12 15:40:53 +0200

    unit commit

--- src/main/java/org/daytime/Main.java
+++ src/main/java/org/daytime/Main.java
@@ -2,6 +2,21 @@ package org.daytime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String [] dayOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        getDayOfWeek(dayOfWeek[0]);
+        getDayOfWeek(dayOfWeek [5]);
+
+    }
+    public static void getDayOfWeek (String weekDay){
+        if (weekDay.equals( "Saturday") || weekDay.equals("Sunday" )) {
+            System.out.print("Day off");
+        }
+        else if (weekDay.equals ("Monday") || weekDay.equals("Tuesday") || weekDay.equals ("Wednesday")
+                || weekDay.equals("Thursday") || weekDay.equals("Friday") )  {
+            System.out.println("Working day");
+        }
+        else {
+            System.out.println("Unknown value");
+        }
     }
 }
